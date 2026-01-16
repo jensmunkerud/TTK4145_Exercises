18cc3bc97a44f01207c58b40c7b9725bfccb5710
Jens <34352934+jensmunkerud@users.noreply.github.com>
2026-01-16 09:27:47 +0100

    added gitignore

--- 5 - bounded buffer/go/main_Jens.go
+++ 5 - bounded buffer/go/main_Jens.go
@@ -1,38 +1,37 @@
-
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
+func producer( /*TODO: parameters?*/ ) {
 
-func producer(/*TODO: parameters?*/){
-
-    for i := 0; i < 10; i++ {
-        time.Sleep(100 * time.Millisecond)
-        fmt.Printf("[producer]: pushing %d\n", i)
-        // TODO: push real value to buffer
-    }
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Printf("[producer]: pushing %d\n", i)
+		// TODO: push real value to buffer
+	}
 
 }
 
-func consumer(/*TODO: parameters?*/){
+func consumer( /*TODO: parameters?*/ ) {
 
-    time.Sleep(1 * time.Second)
-    for {
-        i := 0 //TODO: get real value from buffer
-        fmt.Printf("[consumer]: %d\n", i)
-        time.Sleep(50 * time.Millisecond)
-    }
-    
+	time.Sleep(1 * time.Second)
+	for {
+		i := 0 //TODO: get real value from buffer
+		fmt.Printf("[consumer]: %d\n", i)
+		time.Sleep(50 * time.Millisecond)
+	}
+
 }
 
+func main() {
 
-func main(){
-    
-    // TODO: make a bounded buffer
-    
-    go consumer()
-    go producer()
-    
-    select {}
+	// TODO: make a bounded buffer
+
+	go consumer()
+	go producer()
+
+	select {}
 }
