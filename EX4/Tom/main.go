02e71550b5d9c553f4f6069ec2694167da26a434
Your Name <your.email@example.com>
2026-02-06 09:24:27 +0100

    Added recursive run

--- EX4/Tom/main.go
+++ EX4/Tom/main.go
@@ -1,7 +1,25 @@
+package main
+
 import(
+	"os/exec"
+	"time"
+	"fmt"
 )
 
 func main() {
-
+	fmt.Printf("Sleeping...\n")
+	time.Sleep(10*time.Second)
+	cmd := exec.Command("cmd", "/c", "start", "cmd", "/k", "go run main.go")
+	fmt.Printf("Running command...\n")
+	cmd.Run()
 }
 
+func primary() {
+	fmt.Printf("Sleeping...\n")
+	time.Sleep(10*time.Second)
+}
+
+func backup	() {
+	fmt.Printf("Sleeping...\n")
+	time.Sleep(10*time.Second)
+}
